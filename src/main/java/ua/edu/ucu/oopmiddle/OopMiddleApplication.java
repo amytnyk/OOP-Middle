fbf739cb72c0f768dcf8fc7c3768004cc90c58ed
amytnyk <alex.mytnyk@ukr.net>
2022-11-27 02:13:50 +0200

    heroku

--- src/main/java/ua/edu/ucu/oopmiddle/OopMiddleApplication.java
+++ src/main/java/ua/edu/ucu/oopmiddle/OopMiddleApplication.java
@@ -1,5 +1,6 @@
 package ua.edu.ucu.oopmiddle;
 
+import com.vaadin.flow.component.dependency.JsModule;
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.theme.Theme;
 import com.vaadin.flow.theme.lumo.Lumo;
@@ -9,6 +10,7 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @Theme(variant = Lumo.DARK)
+@JsModule("@vaadin/vaadin-lumo-styles/presets/compact.js")
 public class OopMiddleApplication implements AppShellConfigurator {
 
 	public static void main(String[] args) {
