940c9fc729c13b65abb0821821c80e333cf2916d
amytnyk <alex.mytnyk@ukr.net>
2022-12-02 01:40:21 +0200

    fixed

--- src/main/java/ua/edu/ucu/oopmiddle/core/WebScraper.java
+++ src/main/java/ua/edu/ucu/oopmiddle/core/WebScraper.java
@@ -1,16 +1,12 @@
 package ua.edu.ucu.oopmiddle.core;
 
-import org.springframework.beans.factory.annotation.Autowired;
-
-import javax.servlet.ServletContext;
-
 public class WebScraper {
 
     public CompanyInfo fetchCompanyInfo(String domain) {
 //        Document doc = Jsoup.connect("https://ucu.edu.ua")
 //                .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36")
 //                .get();
-
+//        InputStream stream = VaadinService.getCurrent().getResourceAsStream("frontend/default_logo.png");
         return CompanyInfo.builder()
                 .domain(domain)
                 .name(domain)
@@ -18,7 +14,7 @@ public class WebScraper {
                 .twitterURL("https://twitter.com/" + domain)
                 .facebookURL("https://facebook.com/" + domain)
                 .address("Boston")
-                .logo(getClass().getClassLoader().getResourceAsStream("frontend/default_logo.png"))
+                .logo(getClass().getClassLoader().getResourceAsStream("images/default_logo.png"))
                 .build();
     }
 }
